854ce1c4c427d2486ce9622dd53ad32fa0d6e45e
winde123 <edwinwan88@gmail.com>
2022-02-27 18:38:18 +0800

    adding xpath and realevents to cypress config files

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,6 +15,14 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+require('cypress-xpath');
+import "cypress-real-events/support";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+  })
